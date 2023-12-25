6e16642a4de83c54ccdab29332488cad82bfb8f7
OEbinisa <o.ebinisa@gmail.com>
2023-12-25 01:51:09 +0100

    Compile after commit test

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,18 +1,18 @@
 package com.example;
 
 /**
- * This is a class.
+ * This is a class. ...
  */
 public class Greeter {
 
   /**
-   * This is a constructor.
+   * This is a constructor. ...
    */
   public Greeter() {
 
   }
 
-  //TODO: Add javadoc comment
+  //TODO: Add javadoc comment.
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
